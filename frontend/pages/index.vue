908e12f3aa17128720ab5b26c61470f22c207bb6
Steven Vos <steven@webeau.nl>
2020-02-26 09:29:02 +0100

    Commit
    Add nuxt project

---
+++ frontend/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <span class="white--text">{{ pages }}</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import pagesQuery from '~/assets/apollo/queries/pages/pages'
+
+export default {
+  data () {
+    return {
+      pages: []
+    }
+  },
+  apollo: {
+    pages: {
+      prefetch: true,
+      query: pagesQuery
+    }
+  }
+}
+</script>
