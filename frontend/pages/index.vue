0051d95fe2b7dad562fcee218ec9dbbde623e037
Steven Vos <steven@webeau.nl>
2020-02-28 20:57:53 +0100

    Commit
    Pull info from Strapi

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,8 @@
   <v-container>
     <v-row>
       <v-col>
-        <span class="white--text">{{ pages }}</span>
+        <span class="white--text">{{ pages[0].Title }}</span>
+        <img :src="api_url + pages[0].Photo[0].url" alt="">
       </v-col>
     </v-row>
   </v-container>
@@ -14,7 +15,8 @@ import pagesQuery from '~/assets/apollo/queries/pages/pages'
 export default {
   data () {
     return {
-      pages: []
+      pages: Array,
+      api_url: process.env.strapiBaseUri
     }
   },
   apollo: {
